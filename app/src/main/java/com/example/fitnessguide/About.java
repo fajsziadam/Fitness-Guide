9aa70a1ee86ee5eb9705a7ecd1538f3d813a0a7d
fajsziadam <fajsziadi@gmail.com>
2024-02-26 16:56:59 +0100

    Adding images to imagebuttons

--- app/src/main/java/com/example/fitnessguide/About.java
+++ app/src/main/java/com/example/fitnessguide/About.java
@@ -3,15 +3,18 @@ package com.example.fitnessguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-
-
+        textView = findViewById(R.id.references);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
